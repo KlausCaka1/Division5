f4b8c3d971c6239b3fe679714fb90decf36236c5
Kcaka <kcaka@rash.al>
2020-11-08 12:26:09 +0100

    Division 5 task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {stringify} from '@angular/compiler/src/util';
+import {Observable} from 'rxjs';
+import {UploadService} from './upload.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Divison5';
+  filesUrl: any;
+  imgUrl: any;
+  files = [];
+  images = [];
+  reader = new FileReader();
+
+  constructor(private uploadService: UploadService) {
+  }
+
+  onSelectFile(event): void {
+    if (event.target.files && event.target.files[0]) {
+      this.reader.readAsDataURL(event.target.files[0]);
+      let viewer = '';
+      console.log(stringify(event.target.files[0].name).includes('doc'));
+      const objectURL = window.URL.createObjectURL(event.target.files[0]);
+      if (stringify(event.target.files[0].name).includes('pdf')) {
+        viewer = 'pdf';
+      } else if (stringify(event.target.files[0].name).includes('doc')) {
+        viewer = 'mammoth';
+      } else {
+        viewer = 'google';
+      }
+      const file = {
+        objectURL,
+        viewer,
+      };
+      this.files.push(file);
+    }
+  }
+
+  onSelectImage(event): void {
+    if (event.target.files && event.target.files[0]) {
+      this.reader.readAsDataURL(event.target.files[0]);
+
+      this.reader.onload = (response) => {
+        this.imgUrl = response.target.result;
+        this.images.push(this.imgUrl);
+      };
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTableModule} from '@angular/material/table';
+import {NgxDocViewerModule} from 'ngx-doc-viewer';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatTableModule,
+    NgxDocViewerModule,
+    MatToolbarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
